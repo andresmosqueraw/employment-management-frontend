fe78c68c3f60f6f9ca03950b949f0cff8162780c
Andrés Mosquera <andresmosquera739@gmail.com>
2024-03-10 21:44:08 -0500

    Crear datos en tabla Empleados

--- WPF-LoginForm/Views/HomeView.xaml.cs
+++ WPF-LoginForm/Views/HomeView.xaml.cs
@@ -56,8 +56,69 @@ namespace WPF_LoginForm.Views
             limpiar();
         }
 
+        public bool isValid()
+        {
+            if (nombre_txt.Text == String.Empty)
+            {
+                MessageBox.Show("El campo nombre es obligatorio", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (apellido_txt.Text == String.Empty)
+            {
+                MessageBox.Show("El campo apellido es obligatorio", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (cedula_txt.Text == String.Empty)
+            {
+                MessageBox.Show("El campo cedula es obligatorio", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (correo_txt.Text == String.Empty)
+            {
+                MessageBox.Show("El campo correo es obligatorio", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (salario_txt.Text == String.Empty)
+            {
+                MessageBox.Show("El campo salario es obligatorio", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (cargo_txt.Text == String.Empty)
+            {
+                MessageBox.Show("El campo cargo es obligatorio", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }   
+
         private void btnAgregar_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if (isValid())
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("insert into Empleados (empleado_nombre, empleado_apellido, empleado_cedula, empleado_email, empleado_salario, empleado_cargo, departamento_id) values (@nombre, @apellido, @cedula, @correo, @salario, @cargo, @departamento)", con);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@nombre", nombre_txt.Text);
+                    cmd.Parameters.AddWithValue("@apellido", apellido_txt.Text);
+                    cmd.Parameters.AddWithValue("@cedula", cedula_txt.Text);
+                    cmd.Parameters.AddWithValue("@correo", correo_txt.Text);
+                    cmd.Parameters.AddWithValue("@salario", decimal.Parse(salario_txt.Text));
+                    cmd.Parameters.AddWithValue("@cargo", cargo_txt.Text);
+                    cmd.Parameters.AddWithValue("@departamento", 1);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    CargarDatos();
+                    MessageBox.Show("Empleado agregado correctamente", "Agregado", MessageBoxButton.OK, MessageBoxImage.Information);
+                    limpiar();
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
 
         }
     }
